ea6bbde0d2ae35689922f1780c466f5144b05e75
Kavin Muralikrishnan <921563@lcps.org>
2024-12-17 16:08:02 -0500

    fms

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let teleopButton = document.getElementById("teleop");
-teleopButton.onclick=function(){
-    console.log("runs");
-};
-teleopButton.click();
+let modeButtons = document.getElementsByClassName("mode-button");
+let selectedMode = "";
+for(let i=0; i<modeButtons.length; i++){
+    modeButtons.item(i).onclick = onModeChange;
+}
+modeButtons.item(0).click();
+
+function onModeChange(click){
+    if(selectedMode!==click.target.textContent){
+        selectedMode=click.target.textContent;
+        for(let i=0; i<modeButtons.length; i++){
+            modeButtons.item(i).style.background = "#c9ff00";
+        }
+        click.target.style.background="#00ff1e";
+        switch(click.target.textContent){
+            case "Disabled":
+                
+                break;
+            case "Match":
+
+                break;
+            case "Auto":
+
+                break;
+            case "Teleop":
+
+                break;
+        }
+    }
+}
